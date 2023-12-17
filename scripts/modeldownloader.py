71b3e633113911a55024a4949bfd06ae74ba6c7b
Danil Boldyrev <daswerq123@gmail.com>
2023-12-17 18:17:37 +0300

    update deepspeed uploader

--- scripts/modeldownloader.py
+++ scripts/modeldownloader.py
@@ -20,30 +20,49 @@ def is_package_installed(package_name):
     except metadata.PackageNotFoundError:
         return False
 
+def check_and_install_torch():
+    required_torch_version = 'torch+cu118'
+
+    # Check if torch with CUDA 11.8 is installed.
+    if not any(required_torch_version in pkg for pkg in metadata.distributions()):
+        logger.info(f"'{required_torch_version}' not found. Installing...")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", required_torch_version])
+    else:
+        logger.info(f"'{required_torch_version}' already installed.")
+
 def install_deepspeed_based_on_python_version():
+    # check_and_install_torch()
     if not is_package_installed('deepspeed'):
         python_version = sys.version_info
 
-        logger.info(f"Python version: {python_version}")
-        logger.info("Installing deepspeed package...")
+        logger.info(f"Python version: {python_version.major}.{python_version.minor}")
 
         # Define your package links here
-        py310 = "https://github.com/daswer123/xtts-webui/releases/download/deepspeed/deepspeed-0.11.2+cuda118-cp310-cp310-win_amd64.whl"
-        py311 = "https://github.com/daswer123/xtts-webui/releases/download/deepspeed/deepspeed-0.11.2+cuda118-cp311-cp311-win_amd64.whl"
+        py310_win = "https://github.com/daswer123/xtts-webui/releases/download/deepspeed/deepspeed-0.11.2+cuda118-cp310-cp310-win_amd64.whl"
+        py311_win = "https://github.com/daswer123/xtts-webui/releases/download/deepspeed/deepspeed-0.11.2+cuda118-cp311-cp311-win_amd64.whl"
 
-        # Install appropriate packages based on Python version
-        if python_version.major == 3 and python_version.minor == 10:
-            print("Python 3.10 detected.")
-            install_package(py310)
+        # Use generic pip install deepspeed for Linux or custom wheels for Windows.
+        deepspeed_link = None
 
-        elif python_version.major == 3 and python_version.minor == 11:
-            print("Python 3.11 detected.")
-            install_package(py311)
-        
-        logger.info("Installing deepspeed package...")
+        if sys.platform == 'win32':
+            if python_version.major == 3 and python_version.minor == 10:
+                deepspeed_link = py310_win
 
-    else:
-        print("'deepspeed' already installed.")
+            elif python_version.major == 3 and python_version.minor == 11:
+                deepspeed_link = py311_win
+
+            else:
+                logger.error("Unsupported Python version on Windows.")
+
+        else: # Assuming Linux/MacOS otherwise (add specific checks if necessary)
+             deepspeed_link = 'deepspeed'
+
+        if deepspeed_link:
+             logger.info("Installing DeepSpeed...")
+             install_package(deepspeed_link)
+
+    # else:
+        #  logger.info("'deepspeed' already installed.")
 
 
 
