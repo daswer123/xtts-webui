9e1df0920de4a7cb29fa01bc13ceb4076435b162
Danil Boldyrev <daswerq123@gmail.com>
2023-12-19 05:20:05 +0300

    req fix again

--- install.sh
+++ install.sh
@@ -5,10 +5,9 @@ python -m venv venv
 # Activate the virtual environment
 source venv/bin/activate
 
-# Uncomment the next two lines if you need to install PyTorch and torchaudio with CUDA 11.8 support from PyTorch's wheels.
-pip install torch==2.1.1+cu118 torchaudio==2.1.1+cu118 --index-url https://download.pytorch.org/whl/cu118
 # Install other dependencies from requirements.txt
 pip install -r requirements.txt
+pip install torch==2.1.1+cu118 torchaudio==2.1.1+cu118 --index-url https://download.pytorch.org/whl/cu118
 
 echo "Install deepspeed for Linux for python 3.10.x and CUDA 11.8"
 python scripts/modeldownloader.py
