315382acae3429f42521add99afd3012581c03c9
Danil Boldyrev <daswerq123@gmail.com>
2023-12-26 17:00:43 +0300

    fix

--- scripts/utils/formatter.py
+++ scripts/utils/formatter.py
@@ -158,7 +158,7 @@ def format_audio_list(audio_files, target_language="en", whisper_model = "large-
             else:
                 sentence += word.word
             
-            if word.word[-1] in ["！", "。", "？"]:
+            if word.word[-1] in ["!", "。",".", "?"]:
                 sentence = sentence[1:]
                 # Expand number and abbreviations plus normalization
                 sentence = multilingual_cleaners(sentence, target_language)
